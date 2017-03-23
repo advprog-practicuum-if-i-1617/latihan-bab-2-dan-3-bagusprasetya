52721fae0ee5bb92d8dc2c3433c2e181dfd808d7
Bagus Prasetya <bagusprasetya1997@gmail.com>
2017-03-23 12:48:58 +0700

    Merge branch 'master' of https://github.com/advprog-practicuum-if-i-1617/latihan-bab-2-dan-3-bagusprasetya

--- src/KonversiAngka/konversi.java
+++ src/KonversiAngka/konversi.java
@@ -1,52 +1,69 @@
 package KonversiAngka;
-
 public class konversi {
-    public static void konversi(){
+    private String[] huruf = {
+        "", 
+        "Satu", 
+        "Dua", 
+        "Tiga", 
+        "Empat", 
+        "Lima", 
+        "Enam",
+        "Tujuh", 
+        "Delapan", 
+        "Sembilan", 
+        "Sepuluh", 
+        "Sebelas"};
     
-}
-    public static void konversi(int angka, String val) {
-        String satuan[] = {
-            " ",
-            " Satu",
-            " Dua",
-            " Tiga",
-            " Empat",
-            " lima",
-            " enam",
-            " tujuh",
-            " Delapan",
-            " Sembilan",
-            " Sepuluh",
-            " Sebelas",
-            " Dua Belas",
-            " Tiga Belas",
-            " Empat Belas",
-            " Lima Belas",
-            " Enam Belas",
-            " Tujuh Belas",
-            " Delapan Belas",
-            " Sembilan Belas"
-        };
-        String Puluhan[] = {
-            " ",
-            " ",
-            " Dua Puluh",
-            " Tiga Puluh",
-            " Empat Puluh",
-            " Lima Puluh",
-            " Enam Puluh",
-            " Tujuh Puluh",
-            " Delapan Puluh",
-            " Sembilan Puluh"
-        };
-        if (angka > 19) {
-            System.out.print(Puluhan[angka/10] + " " + satuan[angka % 10]);
+    public void konverter(int satuan) {
+        String hasil = "";
+        if (satuan < 12) {
+            hasil += huruf[satuan];
+        } else if (satuan < 20) {
+            hasil += huruf[satuan - 10] + " belas";
+        } else if (satuan < 100) {
+            hasil += huruf[satuan / 10] + " puluh " + huruf[satuan % 10];
+        } else if (satuan == 100) {
+            hasil += "seratus";
+      //  } else if (satuan < 1000) {
+       //     hasil += huruf[satuan / 100] + " ratus " + huruf[satuan % 100];
+        } else if (satuan == 1000) {
+            hasil += "seribu";
+            
         } else {
-            System.out.print(satuan[angka]);
+            hasil += "\nRange Melibihi Batas Wajar\n";
         }
-        if (angka > 0) {
-            System.out.print(val);
+        System.out.println("Angka " + satuan + " terbilang : " +hasil);
+    }
+    public void konverter(String kalimat) {
+        String[] angka = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"};
+        String[] kata = kalimat.split(" ");
+        String hasil = "";
+        for (int i = 0; i < kata.length; i++) {
+            for (int j = 0; j < angka.length; j++) {
+                if (huruf[j].equalsIgnoreCase(kata[i])) {
+                    hasil += angka[j];
+                } else if ("belas".equalsIgnoreCase(kata[i])) {
+                    hasil = "1" + hasil;
+                } else if ("puluh".equalsIgnoreCase(kata[i]) && kata.length == (kata.length - 1)) {
+                    hasil += "0";
+                } else if ("puluh".equalsIgnoreCase(kata[i])) {
+                } else if (huruf[j].equalsIgnoreCase(kata[i])) {
+                    hasil += angka[j];
+                } else if ("seratus".equalsIgnoreCase(kata[i])) {
+                    hasil = "100";
+                } else if ("seribu".equalsIgnoreCase(kata[i])) {
+                    hasil = "1000";
+                } else if ("nol".equalsIgnoreCase(kata[i])) {
+                    hasil = "0";
+                }
+            }
+        }
+        int hsl = Integer.parseInt(hasil);
+        if (hsl > 100){
+            System.out.println("Terbilang " +kalimat+" :");
+            System.out.println("Range Melibihi Batas Wajar");
+        } else {
+            System.out.println("Terbilang " +kalimat+" : Angka " +hsl);
         }
     }
 }
-
