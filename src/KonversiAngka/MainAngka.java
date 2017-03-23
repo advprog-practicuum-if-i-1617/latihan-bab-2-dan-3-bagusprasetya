9ec8229a819ea4ceb972590a2566a4412d07f6b1
Bagus Prasetya <bagusprasetya1997@gmail.com>
2017-03-23 12:49:34 +0700

    Merge branch 'master' of https://github.com/advprog-practicuum-if-i-1617/latihan-bab-2-dan-3-bagusprasetya

--- src/KonversiAngka/MainAngka.java
+++ src/KonversiAngka/MainAngka.java
@@ -1,32 +1,17 @@
 package KonversiAngka;
-
-import static KonversiAngka.konversi.konversi;
 import java.util.Scanner;
 
 public class MainAngka {
 
     public static void main(String[] args) {
        
-            int num = 0;
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Masukkan Angka (1-1000) : ");
-            num = scanner.nextInt();
-            while (num != -1) {
-                if (num >= 0 && num <= 999) {
-                    if (num == 0) {
-                        System.out.print("Hasil: Nol");
-                    } else {
-                        System.out.print("Hasil: ");
-                        konversi((num / 100) % 1, " Seratus");
-                        konversi(((num / 100) % 10), " Ratus");
-                        konversi((num % 100), " ");
-                    }
-
-                } else {
-                    System.out.print("Angka Terlalu Banyak");
-                }
-                System.out.print("\nMasukkan Angka (1-1000) : ");
-                num = scanner.nextInt();
-            }
-        }
+           konversi convert = new konversi();
+        // isi parameter integer
+        int satuan = 699;
+        // isi parameter String
+        String huruf = "Enam Ratus Tujuh Puluh Tujuh";
+        convert.konverter(satuan);
+        System.out.println("");
+        convert.konverter(huruf);
     }
+}
