36d1e2cdb2dc73982bee0eb07afa3bacd56f9ac0
Bagus Prasetya <bagusprasetya1997@gmail.com>
2017-03-24 01:22:31 +0700

    Merge branch 'master' of https://github.com/advprog-practicuum-if-i-1617/latihan-bab-2-dan-3-bagusprasetya

--- src/KonversiAngka/MainAngka.java
+++ src/KonversiAngka/MainAngka.java
@@ -6,8 +6,8 @@ public class MainAngka {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         konversi convert = new konversi();
-        int satuan = in.nextInt();
-        String huruf = "Sembilan ratus sembilan";
+        int satuan = 500;
+        String huruf = "sembilan ratus sembilan puluh sembilan";
         convert.konverter(satuan);
         System.out.println("");
         convert.konverter(huruf);
